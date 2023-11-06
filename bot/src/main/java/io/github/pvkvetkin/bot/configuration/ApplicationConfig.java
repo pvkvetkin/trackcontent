1120a74695bb9e51750a25154a998ce4820f8231
pvkvetkin <pvkvetkin@gmail.com>
2023-11-06 03:54:22 +0300

    feat: Add application properties

---
+++ bot/src/main/java/io/github/pvkvetkin/bot/configuration/ApplicationConfig.java
@@ -0,0 +1,23 @@
+package io.github.pvkvetkin.bot.configuration;
+
+import jakarta.validation.constraints.NotNull;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+@ConfigurationProperties(prefix = "tgbot", ignoreUnknownFields = false)
+public record ApplicationConfig(
+    @NotNull TgBot bot,
+    @NotNull Scrapper scrapper,
+    @NotNull Rabbitmq rabbitmq
+) {
+
+    record TgBot(String token) {
+    }
+
+    record Scrapper(String url) {
+    }
+
+    public record Rabbitmq(String exchangeName, String queueName) {
+    }
+}
