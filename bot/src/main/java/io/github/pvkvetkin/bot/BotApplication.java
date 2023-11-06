174591f4b4c488d74f9c51b2ed10ce25a607e743
pvkvetkin <pvkvetkin@gmail.com>
2023-11-06 04:07:45 +0300

    feat: Add file with properties for bot

--- bot/src/main/java/io/github/pvkvetkin/bot/BotApplication.java
+++ bot/src/main/java/io/github/pvkvetkin/bot/BotApplication.java
@@ -1,9 +1,12 @@
 package io.github.pvkvetkin.bot;
 
+import io.github.pvkvetkin.bot.configuration.ApplicationConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ApplicationConfig.class)
 public class BotApplication {
 
     public static void main(String[] args) {
