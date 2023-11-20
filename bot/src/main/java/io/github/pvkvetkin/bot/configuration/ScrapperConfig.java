f2fe19173243b73dfea305255cc0eca86331ae8d
pvkvetkin <pvkvetkin@gmail.com>
2023-11-20 23:04:54 +0300

    fix: Make propagation with traceId in other services for bot webclient

--- bot/src/main/java/io/github/pvkvetkin/bot/configuration/ScrapperConfig.java
+++ bot/src/main/java/io/github/pvkvetkin/bot/configuration/ScrapperConfig.java
@@ -3,12 +3,21 @@ package io.github.pvkvetkin.bot.configuration;
 import io.github.pvkvetkin.bot.client.ScrapperClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.Builder;
 
 @Configuration
 public class ScrapperConfig {
 
     @Bean
-    ScrapperClient scrapperClient(ApplicationConfig config) {
-        return new ScrapperClient(config.scrapper().url());
+    WebClient webClient(Builder builder, ApplicationConfig config) {
+        return builder
+            .baseUrl(config.scrapper().url())
+            .build();
+    }
+
+    @Bean
+    ScrapperClient scrapperClient(WebClient webClient) {
+        return new ScrapperClient(webClient);
     }
 }
